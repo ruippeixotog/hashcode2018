875274ce1b7f4f90673d25a62283a3f4f16d298a
Rui Gonçalves <ruippeixotog@gmail.com>
2018-03-01 18:21:31 +0000

    Add code for reading and writing inputs and outputs

--- src/main/scala/hashcode2018/Main.scala
+++ src/main/scala/hashcode2018/Main.scala
@@ -1,5 +1,34 @@
 package hashcode2018
 
+import scala.io.Source
+
+case class Input(vehicleCount: Int, rides: Vector[Ride], bonus: Int, maxSteps: Int)
+case class Ride(from: (Int, Int), to: (Int, Int), minStart: Int, maxEnd: Int)
+
+case class Output(vehicleAssigments: Vector[Vector[Int]])
+
 object Main extends App {
-  println("Hello world")
+
+  def readInput(inputName: String): Input = {
+    val headerLine :: rideLines = Source.fromFile(s"inputs/$inputName.in").getLines().toList
+    val _ :: _ :: vehicleCount :: _ :: bonus :: maxSteps :: Nil = headerLine.split(" ").toList.map(_.toInt)
+
+    val rides = rideLines.map(_.split(" ").toList.map(_.toInt)).map {
+      case fromX :: fromY :: toX :: toY :: minStart :: maxEnd :: Nil =>
+        Ride((fromX, fromY), (toX, toY), minStart, maxEnd)
+    }.toVector
+
+    Input(vehicleCount, rides, bonus, maxSteps)
+  }
+
+  def writeOutput(output: Output): Unit = {
+    output.vehicleAssigments.map { ass => s"${ass.length} ${ass.mkString(" ")}" }.mkString("\n")
+  }
+
+  def doMagic(input: Input): Output = {
+    println(input)
+    Output(Vector.empty)
+  }
+
+  writeOutput(doMagic(readInput("a_example")))
 }
