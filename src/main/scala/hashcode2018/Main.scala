0c9fa3b6ac2231b47dae581f98833f3970937814
Joao Azevedo <joao.c.azevedo@gmail.com>
2018-03-01 18:43:27 +0000

    Add id to Ride

--- src/main/scala/hashcode2018/Main.scala
+++ src/main/scala/hashcode2018/Main.scala
@@ -3,7 +3,7 @@ package hashcode2018
 import scala.io.Source
 
 case class Input(vehicleCount: Int, rides: Vector[Ride], bonus: Int, maxSteps: Int)
-case class Ride(from: (Int, Int), to: (Int, Int), minStart: Int, maxEnd: Int)
+case class Ride(id: Int, from: (Int, Int), to: (Int, Int), minStart: Int, maxEnd: Int)
 
 case class Output(vehicleAssigments: Vector[Vector[Int]])
 
@@ -13,9 +13,9 @@ object Main extends App {
     val headerLine :: rideLines = Source.fromFile(s"inputs/$inputName.in").getLines().toList
     val _ :: _ :: vehicleCount :: _ :: bonus :: maxSteps :: Nil = headerLine.split(" ").toList.map(_.toInt)
 
-    val rides = rideLines.map(_.split(" ").toList.map(_.toInt)).map {
-      case fromX :: fromY :: toX :: toY :: minStart :: maxEnd :: Nil =>
-        Ride((fromX, fromY), (toX, toY), minStart, maxEnd)
+    val rides = rideLines.map(_.split(" ").toList.map(_.toInt)).zipWithIndex.map {
+      case (fromX :: fromY :: toX :: toY :: minStart :: maxEnd :: Nil, id) =>
+        Ride(id, (fromX, fromY), (toX, toY), minStart, maxEnd)
     }.toVector
 
     Input(vehicleCount, rides, bonus, maxSteps)
